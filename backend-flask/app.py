04c5e08bedcccd29e91f158423af7716c9d5c628
dontworryjohn <ohnojay_z@hotmail.it>
2023-03-02 21:25:29 +0000

    Disable cloudwatch code

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -30,13 +30,13 @@ import logging
 from time import strftime
 
 # Configuring Logger to Use CloudWatch
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
-console_handler = logging.StreamHandler()
-cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
-LOGGER.addHandler(console_handler)
-LOGGER.addHandler(cw_handler)
-LOGGER.info("test log")
+#LOGGER = logging.getLogger(__name__)
+#LOGGER.setLevel(logging.DEBUG)
+#console_handler = logging.StreamHandler()
+#cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+#LOGGER.addHandler(console_handler)
+#LOGGER.addHandler(cw_handler)
+#LOGGER.info("test log")
 
 
 # Honeycomb
@@ -113,7 +113,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run(Logger=LOGGER)
+  data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -3,8 +3,9 @@ from opentelemetry import trace
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
-  def run(Logger):
-   Logger.info("HomeActivities")
+  def run():
+   #def run(Logger):
+   #Logger.info("HomeActivities")
    with tracer.start_as_current_span("home-activities-mock-data"):
     span = trace.get_current_span()
     now = datetime.now(timezone.utc).astimezone()
