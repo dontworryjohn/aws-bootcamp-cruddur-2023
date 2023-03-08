68b14a862feda2df56347c0191e9746d5fc91482
dontworryjohn <118534390+dontworryjohn@users.noreply.github.com>
2023-03-08 20:48:37 +0000

    add file postcreatecommand.sh

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,10 @@
+#!/user/bin/env bash
+
+# upgrade npm and install frontend
+cd frontend-react-js && npm update -g && npm i && cd ..
+
+# postgresql
+curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
+sudo apt update
+sudo apt install -y postgresql-client-13 libpq-dev 
