1ee7f56946c500cfacea32d9a01ea4dab3fd6386
dontworryjohn <118534390+dontworryjohn@users.noreply.github.com>
2023-03-11 23:13:55 +0000

    implementation jwt token

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -54,11 +54,11 @@ class HomeActivities:
       if cognito_user_id != None:
         extra_crud = {
           'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-          'handle':  'Lore',
-          'message': 'My dear brother, it the humans that are the problem',
+          'handle':  'Darth Vader',
+          'message': 'I am your father!',
           'created_at': (now - timedelta(hours=1)).isoformat(),
           'expires_at': (now + timedelta(hours=12)).isoformat(),
-          'likes': 1042,
+          'likes': 42,
           'replies': []
         }
         results.insert(0,extra_crud)
