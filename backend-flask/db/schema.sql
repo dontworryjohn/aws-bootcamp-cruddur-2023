e35b5ab602fa2c16ad90cdd06bda0f9cb0cbb9c1
dontworryjohn <ohnojay_z@hotmail.it>
2023-03-14 20:34:22 +0000

    fixed the query for creating activities table

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -3,15 +3,18 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE public.users (
   uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
   display_name text,
-  handle text
+  handle text,
   cognito_user_id text,
   created_at TIMESTAMP default current_timestamp NOT NULL
 );
 
-CREATE TABLE public.users (
+CREATE TABLE public.activities (
   uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-  display_name text,
-  handle text
-  cognito_user_id text,
+  message text NOT NULL,
+  replies_count integer DEFAULT 0,
+  reposts_count integer DEFAULT 0,
+  likes_count integer DEFAULT 0,
+  reply_to_activity_uuid integer,
+  expires_at TIMESTAMP,
   created_at TIMESTAMP default current_timestamp NOT NULL
 );
