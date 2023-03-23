cd034ae34450e593a8deb6672a915c0fb3cd4b7b
dontworryjohn <ohnojay_z@hotmail.it>
2023-03-23 23:55:56 +0000

    updated

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -1,21 +1,77 @@
 from psycopg_pool import ConnectionPool
 import os
 
-def query_wrap_object(template):
-  sql = f"""
-  (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
-  {template}
-  ) object_row);
-  """
-  return sql
+class Db:
+  def __init__(self):
+    self.init_pool()
 
-def query_wrap_array(template):
-  sql = f"""
-  (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
-  {template}
-  ) array_row);
-  """
-  return sql
+  def init_pool():
+    connection_url = os.getenv("CONNECTION_URL")
+    self.pool = ConnectionPool(connection_url)
+  # commit data such as an insert
+  def query_commit():
+    try:
+      conn = self.pool.connection()
+      cur =  conn.cursor()
+      cur.execute(sql)
+      conn.commit()
+    except Exception as err:
+      self.print_sql_err(err)
+      #conn.rollback()
+  # return an array of a json objects
+  def query_array_json():
+    print("SQL STATEMENT---[array]--")
+    print(sql + "\n")
+    wrapped_sql = self.query_wrap_array(sql)
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(wrapped_sql)
+        json = cur.fetchone()
+        return json [0]
+  # return json object
+  def query_object_json(sql):
+    print("SQL STATEMENT---[object]--")
+    print(sql + "\n")
+    wrapped_sql = self.query_wrap_object(sql)
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+          cur.execute(wrapped_sql)
+          json = cur.fetchone()
+          return json [0]
 
-connection_url = os.getenv("CONNECTION_URL")
-pool = ConnectionPool(connection_url)
+  def query_wrap_object(template):
+    sql = f"""
+    (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
+    {template}
+    ) object_row);
+    """
+    return sql
+
+  def query_wrap_array(template):
+    sql = f"""
+    (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
+    {template}
+    ) array_row);
+    """
+    return sql
+    # define a function that handles and parses psycopg2 exceptions
+  def print_sql_err(err):
+    # get details about the exception
+    err_type, err_obj, traceback = sys.exc_info()
+
+    # get the line number when exception occured
+    line_num = traceback.tb_lineno
+
+    # print the connect() error
+    print ("\npsycopg ERROR:", err, "on line number:", line_num)
+    print ("psycopg traceback:", traceback, "-- type:", err_type)
+
+    # psycopg2 extensions.Diagnostics object attribute
+    print ("\nextensions.Diagnostics:", err.diag)
+
+    # print the pgcode and pgerror exceptions
+    print ("pgerror:", err.pgerror)
+    print ("pgcode:", err.pgcode, "\n")
+
+
+db = Db()
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,19 +1,19 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
-from lib.db import pool,query_wrap_array
+from lib.db import db
 
-tracer = trace.get_tracer("home.activities")
+#tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run(cognito_user_id=None):
     #logger.info("HomeActivities")
-    with tracer.start_as_current_span("home-activites-mock-data"):
-      span = trace.get_current_span()
-      now = datetime.now(timezone.utc).astimezone()
-      span.set_attribute("app.now", now.isoformat())
+    #with tracer.start_as_current_span("home-activites-mock-data"):
+     # span = trace.get_current_span()
+      #now = datetime.now(timezone.utc).astimezone()
+      #span.set_attribute("app.now", now.isoformat())
 
   
-      sql = query_wrap_array("""
+      results = db.query_array_json("""
       SELECT
         activities.uuid,
         users.display_name,
@@ -29,13 +29,6 @@ class HomeActivities:
       LEFT JOIN public.users ON users.uuid = activities.user_uuid
       ORDER BY activities.created_at DESC
       """)
-      print(sql)
       #span.set_attribute("app.result_length", len(results))
-      with pool.connection() as conn:
-        with conn.cursor() as cur:
-          cur.execute(sql)
-          # this will return a tuple
-          # the first field being the data
-          json = cur.fetchone()
-      return json[0]
+      return results
     
