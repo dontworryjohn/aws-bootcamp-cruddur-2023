178d3212ddcffd85a37c0854698e8d636dc75ef6
dontworryjohn <ohnojay_z@hotmail.it>
2023-03-25 00:44:31 +0000

    implementation creating activities

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,34 +1,37 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
+
 from lib.db import db
 
-#tracer = trace.get_tracer("home.activities")
+tracer = trace.get_tracer("home-activities")
 
 class HomeActivities:
   def run(cognito_user_id=None):
-    #logger.info("HomeActivities")
-    #with tracer.start_as_current_span("home-activites-mock-data"):
-     # span = trace.get_current_span()
-      #now = datetime.now(timezone.utc).astimezone()
-      #span.set_attribute("app.now", now.isoformat())
-
-  
-    results = db.query_array_json ("""
-      SELECT
-        activities.uuid,
-        users.display_name,
-        users.handle,
-        activities.message,
-        activities.replies_count,
-        activities.reposts_count,
-        activities.likes_count,
-        activities.reply_to_activity_uuid,
-        activities.expires_at,
-        activities.created_at
-      FROM public.activities
-      LEFT JOIN public.users ON users.uuid = activities.user_uuid
-      ORDER BY activities.created_at DESC
-    """)
-      #span.set_attribute("app.result_length", len(results))
-    return results
+    # logger is disabled currently to save on spend
+    # logger.info("home activities")
     
+    # Implementation of traces and spans for HoneyComb
+    with tracer.start_as_current_span("home-activities-mock-data"):
+      span = trace.get_current_span()
+      now = datetime.now(timezone.utc).astimezone()
+      span.set_attribute("app.now", now.isoformat())
+      
+      sql = db.template('activities','home')
+      results = db.query_array_json(sql)
+
+      return results
+      
+      # if cognito_user_id != None:
+      #   extra_crud = {
+      #   'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+      #   'handle':  'Darth Sidious',
+      #   'message': 'Your feeble skills are no match for the power of the Dark Side.',
+      #   'created_at': (now - timedelta(hours=1)).isoformat(),
+      #   'expires_at': (now + timedelta(hours=12)).isoformat(),
+      #   'likes': 0,
+      #   'replies': []
+      # }
+      
+      #   results.insert(0,extra_crud)
+      
+      # return results
