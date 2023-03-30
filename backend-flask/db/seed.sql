bfd382ae623c4b006b8a4d99e0c871a08003a86c
dontworryjohn <118534390+dontworryjohn@users.noreply.github.com>
2023-03-30 17:46:39 +0100

    insert data that has some value on the cognito userpool

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,13 @@
 -- this file was manually created
 INSERT INTO public.users (display_name,email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','johncarlos.buen+ABrown@gmail.com', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko','johncarlos.buen+ABayko@gmail.com', 'bayko' ,'MOCK');
+  ('john carlos buen','johncarlos.buen@gmail.com', 'dontworryjohn' ,'a901d29f-dfb2-4cdd-b14e-8d367e8048d7'),
+  ('john b','ohnojay_z@hotmail.it', 'ohnojohn' ,'d00f9c33-d33f-40b6-a6b4-fab3f4824972');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'dontworryjohn' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
