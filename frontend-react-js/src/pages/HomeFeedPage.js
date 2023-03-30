90af24ffe3b4cb97b8b546f4d2e47d7b58af1cba
dontworryjohn <ohnojay_z@hotmail.it>
2023-03-30 19:19:53 +0000

    create a lib for the checkauth

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -1,12 +1,13 @@
 import './HomeFeedPage.css';
 import React from "react";
-import { Auth } from 'aws-amplify';
+
 
 import DesktopNavigation  from '../components/DesktopNavigation';
 import DesktopSidebar     from '../components/DesktopSidebar';
 import ActivityFeed from '../components/ActivityFeed';
 import ActivityForm from '../components/ActivityForm';
 import ReplyForm from '../components/ReplyForm';
+import checkAuth from '../lib/CheckAuth';
 
 
 export default function HomeFeedPage() {
@@ -37,30 +38,11 @@ export default function HomeFeedPage() {
     }
   };
 
- // check if we are authenicated
-const checkAuth = async () => {
-  Auth.currentAuthenticatedUser({
-    // Optional, By default is false. 
-    // If set to true, this call will send a 
-    // request to Cognito to get the latest user data
-    bypassCache: false 
-  })
-  .then((user) => {
-    console.log('user',user);
-    return Auth.currentAuthenticatedUser()
-  }).then((cognito_user) => {
-      setUser({
-        display_name: cognito_user.attributes.name,
-        handle: cognito_user.attributes.preferred_username
-      })
-  })
-  .catch((err) => console.log(err));
-};
 
 // check when the page loads if we are authenicated
 React.useEffect(()=>{
   loadData();
-  checkAuth();
+  checkAuth(setUser);
 }, [])
 
   React.useEffect(()=>{
