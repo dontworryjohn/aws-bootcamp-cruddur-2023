274f864320b6b7adc6e7d90edb95e1b095936abb
dontworryjohn <ohnojay_z@hotmail.it>
2023-03-31 01:08:40 +0100

    change implementation from @handle to my conversation uuid

--- backend-flask/lib/ddb.py
+++ backend-flask/lib/ddb.py
@@ -15,13 +15,15 @@ class Ddb:
     return dynamodb
 
   def list_message_groups(client,myuser_uuid):
+    current_year = datetime.datetime.now().year
     table_name = 'cruddur-messages'
     query_params = {
       'TableName': table_name,
-      'KeyConditionExpression': 'pk = :pk',
+      'KeyConditionExpression': 'pk = :pk AND begins_with (sk,:year)',
       'ScanIndexForward': False,
       'Limit': 20,
       'ExpressionAttributeValues': {
+        ':year': {'S': year },
         ':pk': {'S': f"GRP#{myuser_uuid}"}
       }
     }
--- frontend-react-js/src/pages/MessageGroupPage.js
+++ frontend-react-js/src/pages/MessageGroupPage.js
@@ -39,8 +39,7 @@ export default function MessageGroupPage() {
 
   const loadMessageGroupData = async () => {
     try {
-      const handle = `@${params.handle}`;
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/messages/${handle}`
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/messages/${params.message_group_uuid}`
       const res = await fetch(backend_url, {
         method: "GET"
       });
