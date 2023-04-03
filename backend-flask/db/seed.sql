9cf7d30624c12d8081869fda6efb777beffc6a62
dontworryjohn <118534390+dontworryjohn@users.noreply.github.com>
2023-04-03 09:45:37 +0000

    implementation new user conversation

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,8 @@
 INSERT INTO public.users (display_name,email, handle, cognito_user_id)
 VALUES
   ('john carlos buen','johncarlos.buen@gmail.com', 'dontworryjohn' ,'a901d29f-dfb2-4cdd-b14e-8d367e8048d7'),
-  ('john b','ohnojay_z@hotmail.it', 'ohnojohn' ,'d00f9c33-d33f-40b6-a6b4-fab3f4824972');
+  ('john b','ohnojay_z@hotmail.it', 'ohnojohn' ,'d00f9c33-d33f-40b6-a6b4-fab3f4824972'),
+  ('Londo Mollari','lmollari@centari.com', 'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -8,6 +8,8 @@ import SigninPage from './pages/SigninPage';
 import RecoverPage from './pages/RecoverPage';
 import MessageGroupsPage from './pages/MessageGroupsPage';
 import MessageGroupPage from './pages/MessageGroupPage';
+import MessageGroupNewPage from './pages/MessageGroupNewPage';
+
 import ConfirmationPage from './pages/ConfirmationPage';
 import React from 'react';
 import {
@@ -50,6 +52,10 @@ const router = createBrowserRouter([
   {
     path: "/messages",
     element: <MessageGroupsPage />
+  },
+  {
+    path: "/messages/new/:handle",
+    element: <MessageGroupNewPage />
   },
   {
     path: "/messages/:message_group_uuid",
