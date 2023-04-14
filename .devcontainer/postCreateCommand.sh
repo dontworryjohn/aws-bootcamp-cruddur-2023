a47835a6098aa4d7f77a25bf95a83e65152bbe0b
dontworryjohn <118534390+dontworryjohn@users.noreply.github.com>
2023-04-14 21:49:23 +0000

    fix code in codespace for the session manager

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -20,5 +20,5 @@ source  "/workspaces/aws-bootcamp-cruddur-2023/backend-flask/bin/rds/update-sg-r
 curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
 sudo dpkg -i session-manager-plugin.deb
 session-manager-plugin
-cd backend-flask
+cd /workspaces/aws-bootcamp-cruddur-2023/backend-flask
 
