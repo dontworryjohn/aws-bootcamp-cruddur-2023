3c8d4f676a1ebf417a5faa762d27882da945bb39
dontworryjohn <ohnojay_z@hotmail.it>
2023-04-24 21:30:26 +0000

    removed some console log

--- frontend-react-js/src/lib/CheckAuth.js
+++ frontend-react-js/src/lib/CheckAuth.js
@@ -1,31 +1,31 @@
 import { Auth } from 'aws-amplify';
+import { resolvePath } from 'react-router-dom';
 
-export async function getAccessToken() {
+export async function getAccessToken(){
   Auth.currentSession()
   .then((cognito_user_session) => {
     const access_token = cognito_user_session.accessToken.jwtToken
     localStorage.setItem("access_token", access_token)
   })
   .catch((err) => console.log(err));
-};
+}
 
-// check if we are authenticated
-export async function checkAuth(setUser) {
-    Auth.currentAuthenticatedUser({
-      // Optional, By default is false. 
-      // If set to true, this call will send a 
-      // request to Cognito to get the latest user data
-      bypassCache: false 
+export async function checkAuth(setUser){
+  Auth.currentAuthenticatedUser({
+    // Optional, By default is false. 
+    // If set to true, this call will send a 
+    // request to Cognito to get the latest user data
+    bypassCache: false 
+  })
+  .then((cognito_user) => {
+    setUser({
+      cognito_user_uuid: cognito_user.attributes.sub,
+      display_name: cognito_user.attributes.name,
+      handle: cognito_user.attributes.preferred_username
     })
-    .then((cognito_user) => {
-      console.log('cognito_user',cognito_user);
-      setUser({
-        display_name: cognito_user.attributes.name,
-        handle: cognito_user.attributes.preferred_username
-      })
-      return Auth.currentSession()
-    }).then((cognito_user_session) => {
+    return Auth.currentSession()
+  }).then((cognito_user_session) => {
       localStorage.setItem("access_token", cognito_user_session.accessToken.jwtToken)
-    })
-    .catch((err) => console.log(err));
-  };
+  })
+  .catch((err) => console.log(err));
+}
