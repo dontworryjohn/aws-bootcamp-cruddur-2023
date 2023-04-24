4a75082a8c4c96988b5d8f0009601891d8effe90
dontworryjohn <ohnojay_z@hotmail.it>
2023-04-24 15:29:17 +0000

    implementation refresh CheckAuth Token

--- frontend-react-js/src/lib/CheckAuth.js
+++ frontend-react-js/src/lib/CheckAuth.js
@@ -1,23 +1,31 @@
 import { Auth } from 'aws-amplify';
 
- // check if we are authenicated
-const checkAuth = async (setUser) => {
-  Auth.currentAuthenticatedUser({
-    // Optional, By default is false. 
-    // If set to true, this call will send a 
-    // request to Cognito to get the latest user data
-    bypassCache: false 
-  })
-  .then((user) => {
-    console.log('user',user);
-    return Auth.currentAuthenticatedUser()
-  }).then((cognito_user) => {
-      setUser({
-        display_name: cognito_user.attributes.name,
-        handle: cognito_user.attributes.preferred_username
-      })
+export async function getAccessToken() {
+  Auth.currentSession()
+  .then((cognito_user_session) => {
+    const access_token = cognito_user_session.accessToken.jwtToken
+    localStorage.setItem("access_token", access_token)
   })
   .catch((err) => console.log(err));
 };
 
-export default checkAuth;
+// check if we are authenticated
+export async function checkAuth(setUser) {
+    Auth.currentAuthenticatedUser({
+      // Optional, By default is false. 
+      // If set to true, this call will send a 
+      // request to Cognito to get the latest user data
+      bypassCache: false 
+    })
+    .then((cognito_user) => {
+      console.log('cognito_user',cognito_user);
+      setUser({
+        display_name: cognito_user.attributes.name,
+        handle: cognito_user.attributes.preferred_username
+      })
+      return Auth.currentSession()
+    }).then((cognito_user_session) => {
+      localStorage.setItem("access_token", cognito_user_session.accessToken.jwtToken)
+    })
+    .catch((err) => console.log(err));
+  };
--- frontend-react-js/src/pages/UserFeedPage.js
+++ frontend-react-js/src/pages/UserFeedPage.js
@@ -6,6 +6,7 @@ import DesktopNavigation  from '../components/DesktopNavigation';
 import DesktopSidebar     from '../components/DesktopSidebar';
 import ActivityFeed from '../components/ActivityFeed';
 import ActivityForm from '../components/ActivityForm';
+import { checkAuth } from '../lib/CheckAuth';
 
 // [TODO] Authenication
 import Cookies from 'js-cookie'
