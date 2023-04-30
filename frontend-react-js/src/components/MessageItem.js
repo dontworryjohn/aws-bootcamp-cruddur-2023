214fe1357f9cabb4705891da85db89a16e976df4
dontworryjohn <ohnojay_z@hotmail.it>
2023-04-30 15:26:27 +0000

    fix push frontend and messageitem for the time zone

--- frontend-react-js/src/components/MessageItem.js
+++ frontend-react-js/src/components/MessageItem.js
@@ -4,22 +4,6 @@ import { format_datetime, message_time_ago } from '../lib/DateTimeFormats';
 
 export default function MessageItem(props) {
 
-  const format_time_created_at = (value) => {
-    // format: 2050-11-20 18:32:47 +0000
-    const created = DateTime.fromISO(value)
-    const now     = DateTime.now()
-    const diff_mins = now.diff(created, 'minutes').toObject().minutes;
-    const diff_hours = now.diff(created, 'hours').toObject().hours;
-
-    if (diff_hours > 24.0){
-      return created.toFormat("LLL L");
-    } else if (diff_hours < 24.0 && diff_hours > 1.0) {
-      return `${Math.floor(diff_hours)}h`;
-    } else if (diff_hours < 1.0) {
-      return `${Math.round(diff_mins)}m`;
-    }
-  };
-
   return (
     <div className='message_item'>
       <Link className='message_avatar' to={`/messages/@`+props.message.handle}></Link>
