5d14f623b7aa406595b5f35cdf62cedc7ebab368
dontworryjohn <ohnojay_z@hotmail.it>
2023-05-16 22:03:44 +0000

    fixed the kill all so it is connditional if the db exists and removed the set -e as it could block the entire script

--- backend-flask/db/kill-all-connections.sql
+++ backend-flask/db/kill-all-connections.sql
@@ -1,14 +1,12 @@
-
-
 DO $$ 
 BEGIN
     IF EXISTS (SELECT 1 FROM pg_database WHERE datname = 'cruddur') THEN
-          SELECT pg_terminate_backend(pid) 
-          FROM pg_stat_activity 
-          WHERE 
-          -- don't kill my own connection!
-          pid <> pg_backend_pid()
-          -- don't kill the connections to other databases
-          AND datname = 'cruddur';
+        RAISE NOTICE 'Killing all the users active!';
+        PERFORM (
+            SELECT pg_terminate_backend(pid) 
+            FROM pg_stat_activity 
+            WHERE pid <> pg_backend_pid()
+            AND datname = 'cruddur'
+        );
     END IF;
 END $$;
