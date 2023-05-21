14d41d99cdaa7c1484931bd23aa97222ff465ae9
dontworryjohn <ohnojay_z@hotmail.it>
2023-05-21 21:27:55 +0100

    added upload and function

--- frontend-react-js/src/components/ProfileForm.js
+++ frontend-react-js/src/components/ProfileForm.js
@@ -13,6 +13,10 @@ export default function ProfileForm(props) {
     setDisplayName(props.profile.display_name);
   }, [props.profile])
 
+  const s3upload = async (event) => {
+
+  }
+
   const onsubmit = async (event) => {
     event.preventDefault();
     try {
@@ -72,6 +76,9 @@ export default function ProfileForm(props) {
             </div>
           </div>
           <div className="popup_content">
+            <div className="upload" onClick={s3upload}>
+              Upload Avatar
+            </div>
             <div className="field display_name">
               <label>Display Name</label>
               <input
