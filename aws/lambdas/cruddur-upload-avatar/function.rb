3a79e3666fd534ed41bb2a874109d624b339e3e3
dontworryjohn <ohnojay_z@hotmail.it>
2023-06-02 01:30:44 +0000

    generate uuid for the image

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -29,13 +29,19 @@ def handler(event:, context:)
     }
   else
     token = event['headers']['authorization'].split(' ')[1]
-
     puts({ step: 'presigned url', access_token: token }.to_json)
+    
+    body_hash = JSON.parse(event["body"])
+    extension = body_hash["extension"]
+
     decoded_token = JWT.decode token, nil, false
-    puts decoded_token.inspect
+    puts decoded_token
+    cognito_user_uuid = decoded_token[0]['sub']
     s3 = Aws::S3::Resource.new
     bucket_name = ENV["UPLOADS_BUCKET_NAME"]
-    object_key = 'mock.jpg'
+    object_key = "#{cognito_user_uuid}.#{extension}"
+
+    puts({object_key: object_key}.to_json)
 
     obj = s3.bucket(bucket_name).object(object_key)
     url = obj.presigned_url(:put, expires_in: 300)
